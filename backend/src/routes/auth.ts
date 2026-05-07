16fcc685ec054816b1c16aae352510c1c4dc2821
chael.chen <michael.chen2510@gmail.com>
2026-05-07 10:54:29 +0700

    Backend : Initialize Login Logic

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { db } from "../db/connection.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const router = express.Router()
 
@@ -23,7 +24,49 @@ router.post("/register", async (req, res) => {
 })
 
 router.post("/login", async (req, res) => {
-  res.json({ message: "login route" })
+  try {
+    const { email, password } = req.body
+
+    const [rows]: any = await db.query(
+      "SELECT * FROM users WHERE email = ?",
+      [email]
+    )
+
+    const user = rows[0]
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      })
+    }
+
+    const validPassword = await bcrypt.compare(
+      password,
+      user.password
+    )
+
+    if (!validPassword) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      })
+    }
+
+    const token = jwt.sign(
+      { id: user.id },
+      "secretkey",
+      { expiresIn: "7d" }
+    )
+
+    res.json({
+      token
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: "Server error"
+    })
+  }
 })
 
 export default router
